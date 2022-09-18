3591931ab180adef506eea63a9e56c80560c5ebf
caboe <ca_boe@yahoo.de>
2022-09-18 17:03:02 +0200

    Add houdini qurey country

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+	import type { PageData } from '$houdini/types/src/routes/$houdini';
+	export let data: PageData;
+
+	$: ({ Country } = data);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h5>{$Country.data?.country?.name}</h5>
